06a44d49530226de00c8d29fe3a2763080149ca6
kacathcart <keithalancathcart@Keiths-MBP.nc.rr.com>
2017-01-10 16:59:38 -0500

    Day 4 Commit on JS Intro

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+var mefavoritenum = 55
+var sum = mefavoritenum + mefavoritenum
+console.log (sum);
+var myPersonalGreeting = "O Keef"
+console.log (myPersonalGreeting);
